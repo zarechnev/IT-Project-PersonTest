c514d26f2988e5628f40e6a696ed4a29ea9d4cf9
Jensen-kaz <kazuhirajob@gmail.com>
2017-09-07 15:11:28 +0700

    init

---
+++ TestApp1/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace TestApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1Question = new System.Windows.Forms.Label();
+            this.richTextBox1QuestionArea = new System.Windows.Forms.RichTextBox();
+            this.label2Variant = new System.Windows.Forms.Label();
+            this.label3ProgressCounter = new System.Windows.Forms.Label();
+            this.label4Extroverted = new System.Windows.Forms.Label();
+            this.label5Introverted = new System.Windows.Forms.Label();
+            this.label6Sensing = new System.Windows.Forms.Label();
+            this.label7Intuitive = new System.Windows.Forms.Label();
+            this.label8Thinking = new System.Windows.Forms.Label();
+            this.label8Feeling = new System.Windows.Forms.Label();
+            this.label9Judging = new System.Windows.Forms.Label();
+            this.label10Perceiving = new System.Windows.Forms.Label();
+            this.label11TypePerson = new System.Windows.Forms.Label();
+            this.richTextBox1Variant1Area = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2Variant2Area = new System.Windows.Forms.RichTextBox();
+            this.button1Variant1 = new System.Windows.Forms.Button();
+            this.button2Variant2 = new System.Windows.Forms.Button();
+            this.richTextBox1PersonDescription = new System.Windows.Forms.RichTextBox();
+            this.label12PersonDescription = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1Question
+            // 
+            this.label1Question.AutoSize = true;
+            this.label1Question.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1Question.Location = new System.Drawing.Point(12, 22);
+            this.label1Question.Name = "label1Question";
+            this.label1Question.Size = new System.Drawing.Size(56, 17);
+            this.label1Question.TabIndex = 0;
+            this.label1Question.Text = "Вопрос";
+            // 
+            // richTextBox1QuestionArea
+            // 
+            this.richTextBox1QuestionArea.Location = new System.Drawing.Point(12, 54);
+            this.richTextBox1QuestionArea.Name = "richTextBox1QuestionArea";
+            this.richTextBox1QuestionArea.Size = new System.Drawing.Size(858, 86);
+            this.richTextBox1QuestionArea.TabIndex = 1;
+            this.richTextBox1QuestionArea.Text = "";
+            // 
+            // label2Variant
+            // 
+            this.label2Variant.AutoSize = true;
+            this.label2Variant.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2Variant.Location = new System.Drawing.Point(12, 172);
+            this.label2Variant.Name = "label2Variant";
+            this.label2Variant.Size = new System.Drawing.Size(464, 17);
+            this.label2Variant.TabIndex = 2;
+            this.label2Variant.Text = "Выберите вариант ответа наиболее подходящий под вашу личность";
+            // 
+            // label3ProgressCounter
+            // 
+            this.label3ProgressCounter.AutoSize = true;
+            this.label3ProgressCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3ProgressCounter.Location = new System.Drawing.Point(702, 22);
+            this.label3ProgressCounter.Name = "label3ProgressCounter";
+            this.label3ProgressCounter.Size = new System.Drawing.Size(129, 17);
+            this.label3ProgressCounter.TabIndex = 3;
+            this.label3ProgressCounter.Text = "Общий прогресс =";
+            // 
+            // label4Extroverted
+            // 
+            this.label4Extroverted.AutoSize = true;
+            this.label4Extroverted.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4Extroverted.Location = new System.Drawing.Point(728, 234);
+            this.label4Extroverted.Name = "label4Extroverted";
+            this.label4Extroverted.Size = new System.Drawing.Size(103, 17);
+            this.label4Extroverted.TabIndex = 4;
+            this.label4Extroverted.Text = "Extroverted(E):";
+            // 
+            // label5Introverted
+            // 
+            this.label5Introverted.AutoSize = true;
+            this.label5Introverted.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5Introverted.Location = new System.Drawing.Point(738, 286);
+            this.label5Introverted.Name = "label5Introverted";
+            this.label5Introverted.Size = new System.Drawing.Size(93, 17);
+            this.label5Introverted.TabIndex = 5;
+            this.label5Introverted.Text = "Introverted(I):";
+            // 
+            // label6Sensing
+            // 
+            this.label6Sensing.AutoSize = true;
+            this.label6Sensing.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6Sensing.Location = new System.Drawing.Point(749, 339);
+            this.label6Sensing.Name = "label6Sensing";
+            this.label6Sensing.Size = new System.Drawing.Size(82, 17);
+            this.label6Sensing.TabIndex = 6;
+            this.label6Sensing.Text = "Sensing(S):";
+            // 
+            // label7Intuitive
+            // 
+            this.label7Intuitive.AutoSize = true;
+            this.label7Intuitive.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7Intuitive.Location = new System.Drawing.Point(751, 365);
+            this.label7Intuitive.Name = "label7Intuitive";
+            this.label7Intuitive.Size = new System.Drawing.Size(80, 17);
+            this.label7Intuitive.TabIndex = 7;
+            this.label7Intuitive.Text = "Intuitive(N):";
+            // 
+            // label8Thinking
+            // 
+            this.label8Thinking.AutoSize = true;
+            this.label8Thinking.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8Thinking.Location = new System.Drawing.Point(746, 313);
+            this.label8Thinking.Name = "label8Thinking";
+            this.label8Thinking.Size = new System.Drawing.Size(85, 17);
+            this.label8Thinking.TabIndex = 8;
+            this.label8Thinking.Text = "Thinking(T):";
+            // 
+            // label8Feeling
+            // 
+            this.label8Feeling.AutoSize = true;
+            this.label8Feeling.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8Feeling.Location = new System.Drawing.Point(755, 391);
+            this.label8Feeling.Name = "label8Feeling";
+            this.label8Feeling.Size = new System.Drawing.Size(76, 17);
+            this.label8Feeling.TabIndex = 9;
+            this.label8Feeling.Text = "Feeling(F):";
+            // 
+            // label9Judging
+            // 
+            this.label9Judging.AutoSize = true;
+            this.label9Judging.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9Judging.Location = new System.Drawing.Point(752, 417);
+            this.label9Judging.Name = "label9Judging";
+            this.label9Judging.Size = new System.Drawing.Size(79, 17);
+            this.label9Judging.TabIndex = 10;
+            this.label9Judging.Text = "Judging(J):";
+            // 
+            // label10Perceiving
+            // 
+            this.label10Perceiving.AutoSize = true;
+            this.label10Perceiving.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10Perceiving.Location = new System.Drawing.Point(734, 260);
+            this.label10Perceiving.Name = "label10Perceiving";
+            this.label10Perceiving.Size = new System.Drawing.Size(97, 17);
+            this.label10Perceiving.TabIndex = 11;
+            this.label10Perceiving.Text = "Perceiving(P):";
+            // 
+            // label11TypePerson
+            // 
+            this.label11TypePerson.AutoSize = true;
+            this.label11TypePerson.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11TypePerson.Location = new System.Drawing.Point(702, 459);
+            this.label11TypePerson.Name = "label11TypePerson";
+            this.label11TypePerson.Size = new System.Drawing.Size(91, 17);
+            this.label11TypePerson.TabIndex = 12;
+            this.label11TypePerson.Text = "Итог теста =";
+            // 
+            // richTextBox1Variant1Area
+            // 
+            this.richTextBox1Variant1Area.Location = new System.Drawing.Point(12, 213);
+            this.richTextBox1Variant1Area.Name = "richTextBox1Variant1Area";
+            this.richTextBox1Variant1Area.Size = new System.Drawing.Size(611, 96);
+            this.richTextBox1Variant1Area.TabIndex = 13;
+            this.richTextBox1Variant1Area.Text = "";
+            // 
+            // richTextBox2Variant2Area
+            // 
+            this.richTextBox2Variant2Area.Location = new System.Drawing.Point(12, 357);
+            this.richTextBox2Variant2Area.Name = "richTextBox2Variant2Area";
+            this.richTextBox2Variant2Area.Size = new System.Drawing.Size(611, 96);
+            this.richTextBox2Variant2Area.TabIndex = 14;
+            this.richTextBox2Variant2Area.Text = "";
+            // 
+            // button1Variant1
+            // 
+            this.button1Variant1.Location = new System.Drawing.Point(413, 315);
+            this.button1Variant1.Name = "button1Variant1";
+            this.button1Variant1.Size = new System.Drawing.Size(210, 23);
+            this.button1Variant1.TabIndex = 15;
+            this.button1Variant1.Text = "Мне подходит первый вариант";
+            this.button1Variant1.UseVisualStyleBackColor = true;
+            // 
+            // button2Variant2
+            // 
+            this.button2Variant2.Location = new System.Drawing.Point(413, 459);
+            this.button2Variant2.Name = "button2Variant2";
+            this.button2Variant2.Size = new System.Drawing.Size(210, 23);
+            this.button2Variant2.TabIndex = 16;
+            this.button2Variant2.Text = "Мне подходит второй вариант";
+            this.button2Variant2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1PersonDescription
+            // 
+            this.richTextBox1PersonDescription.Location = new System.Drawing.Point(921, 54);
+            this.richTextBox1PersonDescription.Name = "richTextBox1PersonDescription";
+            this.richTextBox1PersonDescription.Size = new System.Drawing.Size(396, 422);
+            this.richTextBox1PersonDescription.TabIndex = 17;
+            this.richTextBox1PersonDescription.Text = "";
+            // 
+            // label12PersonDescription
+            // 
+            this.label12PersonDescription.AutoSize = true;
+            this.label12PersonDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12PersonDescription.Location = new System.Drawing.Point(918, 22);
+            this.label12PersonDescription.Name = "label12PersonDescription";
+            this.label12PersonDescription.Size = new System.Drawing.Size(324, 17);
+            this.label12PersonDescription.TabIndex = 18;
+            this.label12PersonDescription.Text = "Описание типа личности по результатам теста";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1333, 505);
+            this.Controls.Add(this.label12PersonDescription);
+            this.Controls.Add(this.richTextBox1PersonDescription);
+            this.Controls.Add(this.button2Variant2);
+            this.Controls.Add(this.button1Variant1);
+            this.Controls.Add(this.richTextBox2Variant2Area);
+            this.Controls.Add(this.richTextBox1Variant1Area);
+            this.Controls.Add(this.label11TypePerson);
+            this.Controls.Add(this.label10Perceiving);
+            this.Controls.Add(this.label9Judging);
+            this.Controls.Add(this.label8Feeling);
+            this.Controls.Add(this.label8Thinking);
+            this.Controls.Add(this.label7Intuitive);
+            this.Controls.Add(this.label6Sensing);
+            this.Controls.Add(this.label5Introverted);
+            this.Controls.Add(this.label4Extroverted);
+            this.Controls.Add(this.label3ProgressCounter);
+            this.Controls.Add(this.label2Variant);
+            this.Controls.Add(this.richTextBox1QuestionArea);
+            this.Controls.Add(this.label1Question);
+            this.MaximumSize = new System.Drawing.Size(1349, 544);
+            this.MinimumSize = new System.Drawing.Size(1349, 544);
+            this.Name = "Form1";
+            this.Text = "Определение типа личности (Гуленко)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1Question;
+        private System.Windows.Forms.RichTextBox richTextBox1QuestionArea;
+        private System.Windows.Forms.Label label2Variant;
+        private System.Windows.Forms.Label label3ProgressCounter;
+        private System.Windows.Forms.Label label4Extroverted;
+        private System.Windows.Forms.Label label5Introverted;
+        private System.Windows.Forms.Label label6Sensing;
+        private System.Windows.Forms.Label label7Intuitive;
+        private System.Windows.Forms.Label label8Thinking;
+        private System.Windows.Forms.Label label8Feeling;
+        private System.Windows.Forms.Label label9Judging;
+        private System.Windows.Forms.Label label10Perceiving;
+        private System.Windows.Forms.Label label11TypePerson;
+        private System.Windows.Forms.RichTextBox richTextBox1Variant1Area;
+        private System.Windows.Forms.RichTextBox richTextBox2Variant2Area;
+        private System.Windows.Forms.Button button1Variant1;
+        private System.Windows.Forms.Button button2Variant2;
+        private System.Windows.Forms.RichTextBox richTextBox1PersonDescription;
+        private System.Windows.Forms.Label label12PersonDescription;
+    }
+}
+
