a59fad80209f2bd71394aec50a959e84313f1ec4
Ruslan A. Zarechnev <zra@corp.zdravalt.ru>
2017-09-07 21:06:11 +0700

    Подправил вопросы и окно проложения.

--- TestApp1/Program.cs
+++ TestApp1/Program.cs
@@ -17,6 +17,11 @@ namespace TestApp1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            var ListOfQuestion = new List<String>();
+
+
+
         }
     }
 }
